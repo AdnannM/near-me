b0cd59967f0e129120bf0e2db2ba416c7c46a138
AdnannM <adnannmuratovic@icloud.com>
2025-05-13 22:40:46 +0200

    Initial Commit

---
+++ Near/NearApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearApp.swift
+//  Near
+//
+//  Created by Adnann Muratovic on 13.05.25.
+//
+
+import SwiftUI
+
+@main
+struct NearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
